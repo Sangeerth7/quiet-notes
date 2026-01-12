ed7d50b58d5748862831bc62507a2cae03400137
Sangeerth7 <sangeerthmurali@gmail.com>
2026-01-12 20:23:48 +0530

    edited style

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function unlock() {
   const input = document.getElementById("passwordInput").value;
   const error = document.getElementById("error");
 
-  if (input === "vibha") {
+  if (input === "nanduvibha") {
     window.location.href = "beginning.html";
   } else {
     error.innerText = "Not for everyone 🤍";
